96e3417b568968f3366ba8d7978450b03ab9ed26
Juldub <j.30@hotmail.fr>
2020-04-25 15:14:49 +0200

    table rase

--- src/package/components/commons/avatar/avatar.jsx
+++ src/package/components/commons/avatar/avatar.jsx
@@ -11,7 +11,7 @@ import { styles } from './avatar_styles';
 const useStyles = createUseStyles(styles);
 
 // test
-const DEFAULT_IMAGE = 'http://juliendubo.is/DSC_0036.jpg';
+const DEFAULT_IMAGE = '//juliendubo.is/DSC_0036.jpg';
 const AvatarComponent = ({ src, displayedName }) => {
     const classes = useStyles();
     const [receivedGlobalClasses] = useReceivedGlobalClasses('banner.avatar');
