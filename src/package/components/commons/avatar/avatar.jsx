a0dc46073fd81507c8b5a21aa71e027aa2bebf25
Juldub <j.30@hotmail.fr>
2020-05-01 15:28:52 +0200

    passage des images sur le site de julietacontrras.pro

--- src/package/components/commons/avatar/avatar.jsx
+++ src/package/components/commons/avatar/avatar.jsx
@@ -11,7 +11,7 @@ import { styles } from './avatar_styles';
 const useStyles = createUseStyles(styles);
 
 // test
-const DEFAULT_IMAGE = '//juliendubo.is/photo-julieta.jpg';
+const DEFAULT_IMAGE = '//julietacontreras.pro/images/photo-julieta.jpg';
 const AvatarComponent = ({ src, displayedName }) => {
     const classes = useStyles();
     const [receivedGlobalClasses] = useReceivedGlobalClasses('banner.avatar');
